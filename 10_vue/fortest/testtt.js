943e5abcf5f19cc4155b75a759e2d840270b483c
leesangju92 <leesangju92@gmail.com>
2019-05-03 17:54:33 +0900

    0503

---
+++ 10_vue/fortest/testtt.js
@@ -0,0 +1,5 @@
+const LSJ = require("myfirstpubbbbbb");
+
+console.log(LSJ.utility.name);
+
+console.log(LSJ.utility.addAll([1,2,3,4]));
---
+++ 10_vue/node.js
@@ -0,0 +1,11 @@
+const aaa = require("fortest/myfirstpubbbbbb");
+
+console.log(aaa.name);
+
+
+// global
+
+// process
+
+//
+global.console.log(process.env);
